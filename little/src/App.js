08863b47a17a73c7ffbee656f9b3774d5bf2225e
Antonio Jimenez <ajimeneza77@gmail.com>
2023-01-16 10:51:04 -0800

    added metadata and logo to image files

--- little/src/App.js
+++ little/src/App.js
@@ -1,4 +1,4 @@
-
+import { Helmet,HelmetProvider } from 'react-helmet-async';
 import './App.css';
 import {Header} from './Components/Header';
 import {Nav} from './Components/Nav';
@@ -7,12 +7,29 @@ import {Footer} from './Components/Footer';
 
 function App() {
   return (
+    <HelmetProvider>
+      <Helmet>
+        <meta name="description" content="Little Lemon Restaurant"/>
+      </Helmet>
+      <Helmet>
+      <meta name="og:title" content="Little Lemon Restaurant"/>
+      </Helmet>
+      <Helmet>
+      <meta name="og:description" content="Try our unique dishes"/>
+      </Helmet>
+      <Helmet>
+      <meta name="og:image" content="./images/Logo .svg"/>
+      </Helmet>
+
+
+    
     <>
     <Header/>
     <Nav/>
     <Main/>
     <Footer/>
     </>
+    </HelmetProvider>
   );
 }
 
