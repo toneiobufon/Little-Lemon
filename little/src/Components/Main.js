33e4ace31dbb91eff534f46cdaa9637eefe6ac29
Antonio Jimenez <ajimeneza77@gmail.com>
2023-01-19 19:30:03 -0800

    refactored specials menu

--- little/src/Components/Main.js
+++ little/src/Components/Main.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Greeksalad from '../images/Greeksalad.jpg'
+import bruchetta from '../images/bruchetta.svg'
+import LemonDessert from '../images/LemonDessert.jpg'
 
 export default function Main(){
     return(
@@ -12,7 +15,7 @@ export default function Main(){
         </div>
         <section>
             <article>
-                <img src="../images/greek salad.jpg" alt="Greek Salad"/>
+                <img src={Greeksalad} alt="Greek Salad"/>
                 <h1>Greek Salad       $12.99</h1>
                 <p>The famous greek salad of crispy lettuce, peppers, 
                     olives and our Chicago style feta cheese, garnished with 
@@ -20,14 +23,14 @@ export default function Main(){
                 <button>Order a delivery</button>
             </article>
             <article>
-                <img src="../images/bruchetta.svg" alt="Bruchetta"/>
+                <img className="bruchetta" src={bruchetta} alt="Bruchetta"/>
                 <h1>Bruchetta    $5.99</h1>
                 <p>Our Bruschetta is made from grilled bread that has been
                 smeared with garlic and seasoned with salt and olive oil. </p>
                 <button>Order a delivery</button>
             </article>
             <article>
-                <img src="../images/lemon dessert.jpg" alt="Lemon Dessert"/>
+                <img src={LemonDessert} alt="Lemon Dessert"/>
                 <h1>Lemon Dessert  $4.99</h1>
                 <p>This comes straight from grandma’s recipe book, 
                     every last ingredient has been sourced and is as 
