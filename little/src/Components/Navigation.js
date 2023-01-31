46656f77621a6bd5abb6feef4518d0c78b3f3dd9
Antonio Jimenez <ajimeneza77@gmail.com>
2023-01-31 09:53:40 -0800

    refactored navbar and added X when hamburguer menu is clicked

--- little/src/Components/Navigation.js
+++ little/src/Components/Navigation.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Logo from '../images/Logo .svg'
-import {Nav, Navbar} from 'react-bootstrap';
+import {Nav, Navbar,Container,Offcanvas} from 'react-bootstrap';
 
 
 
@@ -11,25 +11,68 @@ export default function Navigation(){
 
 
     return(
-    
-        
-       <Navbar className="Navbar" collapseOnSelect expand="lg"> 
-            <a href="/">
-                <img src={Logo} alt="Little Lemon" />
-            </a>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-            <Navbar.Collapse id="responsive-navbar-nav" >
-                <Nav className="ms-auto">
+
+        <>
+        {['lg'].map((expand) => (
+          <Navbar key={expand}  expand={expand} className="mb-3">
+            <Container fluid>
+              <Navbar.Brand href="#">
+                <a href="/">
+                    <img src={Logo} alt="Little Lemon" />
+                </a>
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+              <Navbar.Offcanvas
+                id={`offcanvasNavbar-expand-${expand}`}
+                aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                placement="end"
+              >
+                <Offcanvas.Header closeButton>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                  <Nav className="justify-content-end flex-grow-1 pe-3">
                     <Nav.Link href="/">Home</Nav.Link>
                     <Nav.Link href="/about">About</Nav.Link>
                     <Nav.Link href="/menu">Menu</Nav.Link>
                     <Nav.Link href="/reservation">Reservation</Nav.Link>
                     <Nav.Link href="/orderonline">Order Online</Nav.Link>
                     <Nav.Link href="/login">Login</Nav.Link>
-
-                </Nav>
-            </Navbar.Collapse>
+                 
+                  </Nav>
+                  
+                </Offcanvas.Body>
+              </Navbar.Offcanvas>
+            </Container>
+          </Navbar>
+        ))}
+      </>
+       
     
-       </Navbar>        
+        
+    //    <Navbar  className="Navbar" collapseOnSelect expand="lg"> 
+    //         <a href="/">
+    //             <img src={Logo} alt="Little Lemon" />
+    //         </a>
+    //         <Navbar.Toggle aria-controls='responsive-navbar-nav' closeButton>
+               
+    //         </Navbar.Toggle>
+                
+        
+    //         <Navbar.Collapse id="responsive-navbar-nav" >
+    //             <Nav className="ms-auto">
+    //                 <Nav.Link href="/">Home</Nav.Link>
+    //                 <Nav.Link href="/about">About</Nav.Link>
+    //                 <Nav.Link href="/menu">Menu</Nav.Link>
+    //                 <Nav.Link href="/reservation">Reservation</Nav.Link>
+    //                 <Nav.Link href="/orderonline">Order Online</Nav.Link>
+    //                 <Nav.Link href="/login">Login</Nav.Link>
+
+    //             </Nav>
+    //         </Navbar.Collapse>
+    
+    //    </Navbar>        
+    
+    
     )
+   
 }
