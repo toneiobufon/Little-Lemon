9b8d76fa1cb0cf48317e4cb1986cf30cbdf70355
Antonio Jimenez <ajimeneza77@gmail.com>
2023-03-15 10:51:33 -0700

    added lorem to about us page

--- little/src/Components/About.js
+++ little/src/Components/About.js
@@ -4,8 +4,11 @@ import React from "react";
 function About (){
     return (
         <>
-    <h1>About ussss page</h1>
-    <p></p>
+    <h1>About us</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos qui veniam cumque eligendi ex, assumenda delectus laborum dolorem illum sit corrupti vero, atque veritatis harum cum saepe hic odio. Sit.</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur cum nam neque perferendis nostrum labore, recusandae temporibus dicta repellat magni, dolorum ea voluptatibus dolor nulla nisi sed odio quia? Eum?</p>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis illo tenetur eaque incidunt pariatur expedita labore. Minima alias aliquid, fugit totam magnam, cumque optio praesentium officiis obcaecati nesciunt tenetur voluptatibus.</p>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo, voluptas officiis. Explicabo obcaecati sint eveniet dolorum, dolore tempora blanditiis vero quidem voluptatem odit mollitia error totam fuga optio vitae provident.</p>
     </>
     )
 }
